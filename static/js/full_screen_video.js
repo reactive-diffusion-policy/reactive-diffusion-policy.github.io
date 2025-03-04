4b03021e88785049bdc8246c4d338cefe311fcb0
WendiChen <1277998401@qq.com>
2025-03-04 15:21:12 +0800

    feat: show bg-video-speed after bg-video is loaded

--- static/js/full_screen_video.js
+++ static/js/full_screen_video.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const videoElement = document.getElementById('bg-video');
+    const videoSpeedElement = document.getElementById('bg-video-speed');
 
     // When the video metadata is loaded
     videoElement.addEventListener('loadedmetadata', function () {
@@ -7,5 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
         // videoElement.play();
         // Gradually fade in the video after it finishes loading
         videoElement.style.opacity = 1.0;
+        videoSpeedElement.style.opacity = 1.0;
     });
 });
